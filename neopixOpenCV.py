0a9936bd5f3951432a46da8c00fddbbeed865d97
ydeydeyde <35302563+ydeydeyde@users.noreply.github.com>
2018-01-21 09:40:07 +0000

    Update neopixOpenCV.py

--- neopixOpenCV.py
+++ neopixOpenCV.py
@@ -1,12 +1,13 @@
 import time
  
 from neopixel import *
- 
+import numpy as np
+import cv2
 import argparse
 import signal
 import sys
+import random
 def signal_handler(signal, frame):
-        colorWipe(strip, Color(0,0,0))
         sys.exit(0)
  
 def opt_parse():
@@ -15,10 +16,13 @@ def opt_parse():
         args = parser.parse_args()
         if args.c:
                 signal.signal(signal.SIGINT, signal_handler)
- 
+                
 # LED strip configuration:
-LED_COUNT      = 16      # Number of LED pixels.
+LED_COUNT      = 180      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN_2      = 17      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN_3      = 16      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN_4      = 22      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
@@ -26,87 +30,63 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
- 
- 
- 
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
- 
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
- 
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
- 
-def rainbow(strip, wait_ms=20, iterations=1):
-    """Draw rainbow that fades across all pixels at once."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((i+j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
- 
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-    """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
- 
-def theaterChaseRainbow(strip, wait_ms=50):
-    """Rainbow movie theater light style chaser animation."""
-    for j in range(256):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, wheel((i+j) % 255))
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
- 
-# Main program logic follows:
+
+
+
+cap = cv2.VideoCapture(0)
+fgbg = cv2.createBackgroundSubtractorMOG2(history=10,varThreshold=100,detectShadows=False)
+
+def singlePix(i, color):
+    strip.setPixelColor(i,  color)
+    strip.show()
+    time.sleep(1/1000.0) 
+    
 if __name__ == '__main__':
-        # Process arguments
         opt_parse()
- 
-    # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
- 
-    print ('Press Ctrl-C to quit.')
-    while True:
-        print ('Color wipe animations.')
-        colorWipe(strip, Color(255, 0, 0))  # Red wipe
-        colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-        colorWipe(strip, Color(0, 0, 255))  # Green wipe
-        print ('Theater chase animations.')
-        theaterChase(strip, Color(127, 127, 127))  # White theater chase
-        theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-        theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-        print ('Rainbow animations.')
-        rainbow(strip)
-        rainbowCycle(strip)
-        theaterChaseRainbow(strip)
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+	strip.begin()
+	#strip_2 = Adafruit_NeoPixel(LED_COUNT, LED_PIN_2, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+	#strip_2.begin()
+	#strip_3 = Adafruit_NeoPixel(LED_COUNT, LED_PIN_3, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+	#strip_3.begin()
+	#strip_4 = Adafruit_NeoPixel(LED_COUNT, LED_PIN_4, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+	#strip_4.begin()
+	print ('Press Ctrl-C to quit.')
+	
+while True: 
+    ret, frame = cap.read()
+    if ret == False:
+        print("no reading")
+    else:
+        k = cv2.waitKey(25) & 0xff 
+        if k == 27:
+            break
+        fgmask = fgbg.apply(frame)
+        fgmask = cv2.flip(fgmask, 1)
+        fgmask = fgmask[235:245,0:360]
+        # kernel = np.ones((2,2),np.uint8)
+        #fgmask = cv2.erode(fgmask,kernel,iterations = 1)
+        #fgmask = cv2.dilate(fgmask, kernel, iterations = 1)
+        # cv2.rectangle(fgmask,(0,235),(300,245),(0,255,0),3)
+        # cv2.imshow('frame', frame)
+        cv2.imshow('frame',fgmask)
+
+        h = int(time.time() % 6)
+        r = h * 51
+        b = 255 - r
+        for i in range (0, 120):
+            if fgmask[5, i*3] > 5:
+                strip.setPixelColor(i,  Color(r,0,b))
+                strip.show()
+                #singlePix(i, Color(255,0,0))
+                #time.sleep(5/ 1000)
+            # else :
+                # strip.setPixelColor(i,  Color(0,0,0))
+                # strip.show() a
+                # singlePix(i, Color(0,0,255))
+                #time.sleep(5/ 1000)
+            
+cap.release()
+cv2.destroyAllWindows()
+
+
